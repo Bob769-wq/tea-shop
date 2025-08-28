c9aa2d8d719c10db840eff8617191d9d1e256c37
Bob <dash78231@gmail.com>
2025-08-28 13:42:26 +0800

    right-corner 動畫 加上半圓形

--- src/app/right-corner.component.ts
+++ src/app/right-corner.component.ts
@@ -9,23 +9,19 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
       <button
         class="w-16 h-16 bg-green-500 rounded-full rounded-ee-none flex justify-center items-center group relative"
       >
-        <svg
-          class="w-8 h-8 text-white group-hover:hidden transition-all duration-300"
-          fill="currentColor"
-          viewBox="0 0 24 24"
-        >
-          <circle cx="5" cy="12" r="2" />
-          <circle cx="12" cy="12" r="2" />
-          <circle cx="19" cy="12" r="2" />
-        </svg>
-        <svg
-          class="w-8 h-8 text-white hidden transition-all duration-300 ease-in-out group-hover:block"
-          fill="currentColor"
-          viewBox="0 0 24 24"
-        >
-          <circle cx="5" cy="12" r="2" />
-          <circle cx="12" cy="20" r="2" />
-          <circle cx="19" cy="12" r="2" />
+        <svg class="w-8 h-8 text-white" fill="currentColor" viewBox="0 0 24 24">
+          <circle cx="5" cy="12" r="3" class="transition-all duration-300 ease-out" />
+          <circle
+            cx="12"
+            cy="12"
+            r="3"
+            class="transition-all duration-300 ease-out group-hover:opacity-0 group-hover:translate-y-2"
+          />
+          <path
+            d="M 9 18 A 3 3 0 0 0 15 18 Z"
+            class="transition-all duration-300 ease-out opacity-0 -translate-y-2 group-hover:opacity-100 group-hover:translate-y-0"
+          />
+          <circle cx="19" cy="12" r="3" class="transition-all duration-300 ease-out" />
         </svg>
       </button>
     </div>
